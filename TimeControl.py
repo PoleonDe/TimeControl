921f006d5f8ab40811d798dc36bafafb98a102de
MalteDe <malte1998@hotmail.de>
2023-11-04 00:49:26 +0100

    First Version that works fine.

--- TimeControl.py
+++ TimeControl.py
@@ -1,4 +1,6 @@
 import bpy
+import time
+from threading import Timer
 
 bl_info = {
     "name": "Time Control",
@@ -6,17 +8,40 @@ bl_info = {
     "author": "Blendercontrol - Blendercontrol@gmail.com",
     "version": (0, 0, 1),
     "blender": (3, 6, 0),
-    "location": "Shortcuts : Shift E and E",
-    "category": "Lighting" # TODO : Test if "Time"  is a Category
+    "location": "Will automatically be triggered when Blender Starts",
+    "category": "Time"
 }
 
+timerFunction = None
+
+def InfiniteTimer():
+    bpy.context.preferences.addons[__name__].preferences.totalOpenTime += 1.0
+    print("TICK " + str(bpy.context.preferences.addons[__name__].preferences.totalOpenTime))
+    return 1.0
+
+def StartTimerFunc():
+    print("START")
+    global timerFunction
+    timerFunction = InfiniteTimer
+    bpy.app.timers.register(timerFunction)
+
+def StopTimerFunc():
+    print("STOP")
+    global timerFunction
+    print("timer function is : " + str(timerFunction))
+    if bpy.app.timers.is_registered(timerFunction):
+        print("is Registered")
+    else:
+        print("is Not Registered")
+    bpy.app.timers.unregister(timerFunction)
 
 # def draw_popover(self,context):
 #     row : bpy.types.UILayout  = self.layout.row()
 #     row = row.row(align=True)
 #     row.operator('lightcontrol.adjust_light', text='Edit', icon='OUTLINER_OB_LIGHT',)
 
-class LIGHTCONTROL_Addon_Preferences(bpy.types.AddonPreferences):
+
+class TIMECONTROL_Addon_Preferences(bpy.types.AddonPreferences):
     # this must match the add-on name, use '__package__'
     # when defining this in a submodule of a python package.
     bl_idname = __name__
@@ -26,8 +51,7 @@ class LIGHTCONTROL_Addon_Preferences(bpy.types.AddonPreferences):
     #     if self.showEditLightButtonPanel:
     #         bpy.types.VIEW3D_MT_editor_menus.append(draw_popover)
             
-
-    currentTime: bpy.props.FloatProperty(
+    totalOpenTime: bpy.props.FloatProperty(
         name="File Open Time",
         description="Time this Blender is Open",
         default=0.0
@@ -35,62 +59,24 @@ class LIGHTCONTROL_Addon_Preferences(bpy.types.AddonPreferences):
 
     def draw(self, context):
         layout: bpy.types.UILayout = self.layout
-        layout.label(text="File is open for: " + str(self.currentTime) + " Seconds")
+        layout.label(text="Blender is open for: " + str(self.totalOpenTime) + " Seconds since registration of this Addon")
 
 
 #################################################################
 ####################### REGISTRATION ############################
 #################################################################
 
-classes = (LIGHTCONTROL_Addon_Preferences)
-
-
 def register():
     print("registered")
-    for cls in classes:
-        bpy.utils.register_class(cls)
-
-    #bpy.data.window_managers[0]["someProperty"]
-    bpy.types.Scene.someProperty = bpy.props.FloatProperty(name="someProperty", description="bla bla", default=0.0)
-    #if 'someProperty' in bpy.data.window_managers[0]:
-
-    
-    # spawnEffectTimer = 0.0
-    # spawnEffectDuration = 0.4
-    # spawnEffectTickTime = 0.033
-
-    # def invoke(self, context: bpy.types.Context, event: bpy.types.Event):
-    #     # Addon Preferences
-    #     preferences = context.preferences
-    #     addon_prefs = preferences.addons[__name__].preferences
-
-    #     def drawSpawnEffect():
-    #         if self.spawnEffectTimer == 0:
-    #             #DO SOMETHING ONCE
-    #             pass
-
-    #         #ON TICK UPDATE
-    #         self.spawnEffectTimer += self.spawnEffectTickTime
-    #         self.area3D.tag_redraw()
-
-    #         if self.spawnEffectTimer >= self.spawnEffectDuration:
-    #             # BREAK
-    #             return None
-    #         return self.spawnEffectTickTime
-
-    #     # register drawEffect
-    #     bpy.app.timers.register(drawSpawnEffect)
-
-    #bpy.types.VIEW3D_MT_editor_menus.append(draw_popover)
-
+    bpy.utils.register_class(TIMECONTROL_Addon_Preferences)
+    Timer(1, StartTimerFunc, ()).start()
 
 def unregister():
+    Timer(1, StopTimerFunc, ()).start()
+    bpy.utils.unregister_class(TIMECONTROL_Addon_Preferences)
     print("unregistered")
-    for cls in classes:
-        bpy.utils.unregister_class(cls)
 
     #bpy.types.VIEW3D_MT_editor_menus.remove(draw_popover)
-
 
 if __name__ == "__main__":
     register()
