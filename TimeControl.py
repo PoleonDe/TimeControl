8096ee06c9961a2b0144ae49defe46f8f9feac91
MalteDe <malte1998@hotmail.de>
2023-11-04 19:31:38 +0100

    Added Scene Timing to Addon

--- TimeControl.py
+++ TimeControl.py
@@ -1,6 +1,5 @@
 import bpy
 from threading import Timer
-import random
 
 bl_info = {
     "name": "Time Control",
@@ -16,62 +15,49 @@ timerFunction = None
 
 def InfiniteTimer():
     bpy.context.preferences.addons[__name__].preferences.totalOpenTime += 1.0
-    print("TICK " + str(bpy.context.preferences.addons[__name__].preferences.totalOpenTime))
+    bpy.context.scene.timecontrol_timer += 1.0
+    print("TICK TOTAL" + str(bpy.context.preferences.addons[__name__].preferences.totalOpenTime))
+    print("TICK SCENE" + str(bpy.context.scene.timecontrol_timer))
     return 1.0
 
-def StartTimerFunc():
-    print("START")
-    item = bpy.context.preferences.addons[__name__].preferences.sceneTimes.add()
-    item.name = "randomName"
-    item.id = "someID"
-    item.time = 1231.2
-    print("NEW ELEMENT " + str(item.time) + str(item.id))
 
-    #something: bpy.types.FloatProperty = bpy.props.FloatProperty(name="")
-    #something.g
-    print([method_name for method_name in dir(bpy.context.preferences.addons[__name__].preferences.sceneTimes[0].time.__getattribute__)if callable(getattr(bpy.context.preferences.addons[__name__].preferences.sceneTimes[0].time.__getattribute__, method_name))])
-    print(bpy.context.preferences.addons[__name__].preferences.sceneTimes[0].time.__getattribute__)
+def StartTimerWrapper():
+    print("START WRAPPER")
+    bpy.ops.timecontrol.start_timer()
 
-    # if "randomName" in bpy.context.preferences.addons[__name__].preferences.sceneTimes:
-    #     print("IS FOUND")
-    #     bpy.context.preferences.addons[__name__].preferences.sceneTimes["randomName"].time.set(1.0)
-    #     print(bpy.context.preferences.addons[__name__].preferences.sceneTimes["randomName"].time)
+class TIMECONTORL_start_timer(bpy.types.Operator):
+    bl_idname = "timecontrol.start_timer"
+    bl_label = "Start Time Control Timer"
 
-    #     print("ACCESS " + str(bpy.context.preferences.addons[__name__].preferences.sceneTimes["randomName"].time))
-    # else:
-    #     print("NOT FOUND")
 
-    # print("Scene Time Group = " + str(bpy.context.preferences.addons[__name__].preferences.sceneTimes))
+    def execute(self, context):
+        global timerFunction
+        if bpy.app.timers.is_registered(timerFunction): # return if already registered
+            return {'FINISHED'}
+        
+        if "timecontrol_timer" not in context.scene.keys():
+            bpy.types.Scene.timecontrol_timer = bpy.props.FloatProperty(name="timecontrol_timer", description="Mouse Position when Pie Menu was Initialized",default=0.0)
+            context.scene["timecontrol_timer"] = 0.0
 
-    # for element in bpy.context.preferences.addons[__name__].preferences.sceneTimes:
-    #     print(element)
-    #     print(element.time)
+        timerFunction = InfiniteTimer
+        bpy.app.timers.register(timerFunction)
+        return {'FINISHED'}
 
-    # someL : CustomGroup = None
-    # someL.
 
-    global timerFunction
-    timerFunction = InfiniteTimer
-    bpy.app.timers.register(timerFunction)
+class TIMECONTORL_stop_timer(bpy.types.Operator):
+    bl_idname = "timecontrol.stop_timer"
+    bl_label = "Stop Time Control Timer"
 
-def StopTimerFunc():
-    print("STOP")
-    global timerFunction
-    print("timer function is : " + str(timerFunction))
-    if bpy.app.timers.is_registered(timerFunction):
-        print("is Registered")
-    else:
-        print("is Not Registered")
-    bpy.app.timers.unregister(timerFunction)
+    def execute(self, context):
+        global timerFunction
+        if bpy.app.timers.is_registered(timerFunction):
+            bpy.app.timers.unregister(timerFunction)
+        return {'FINISHED'}
 
 # def draw_popover(self,context):
 #     row : bpy.types.UILayout  = self.layout.row()
 #     row = row.row(align=True)
 #     row.operator('lightcontrol.adjust_light', text='Edit', icon='OUTLINER_OB_LIGHT',)
-
-class CustomGroup(bpy.types.PropertyGroup):
-    id = bpy.props.StringProperty(name="someID", default="")
-    time = bpy.props.FloatProperty(name = "someTime", default=0.0)
 
 class TIMECONTROL_Addon_Preferences(bpy.types.AddonPreferences):
     # this must match the add-on name, use '__package__'
@@ -88,19 +74,10 @@ class TIMECONTROL_Addon_Preferences(bpy.types.AddonPreferences):
         description="Time this Blender is Open",
         default=0.0
     )
-    
-    sceneTimes: bpy.props.CollectionProperty(
-        type = CustomGroup,
-        name = "Scene Times", 
-        description = "Saves all Scene Times"
-    )
-    
 
     def draw(self, context):
         layout: bpy.types.UILayout = self.layout
         layout.label(text="Blender is open for: " + str(self.totalOpenTime) + " Seconds since registration of this Addon")
-        for e in self.sceneTimes:
-            layout.label(text=str(e.time))
 
 
 
@@ -108,21 +85,22 @@ class TIMECONTROL_Addon_Preferences(bpy.types.AddonPreferences):
 ####################### REGISTRATION ############################
 #################################################################
 
-classes = [CustomGroup,TIMECONTROL_Addon_Preferences]
+classes = [TIMECONTORL_start_timer, TIMECONTORL_stop_timer, TIMECONTROL_Addon_Preferences]
 
 def register():
     print("registered")
     for cls in classes:
         bpy.utils.register_class(cls)
-    Timer(1, StartTimerFunc, ()).start()
+
+    Timer(1, StartTimerWrapper, ()).start()
 
 def unregister():
-    Timer(1, StopTimerFunc, ()).start()
+    global timerFunction
+    if bpy.app.timers.is_registered(timerFunction):
+        bpy.app.timers.unregister(timerFunction)
+
     for cls in classes:
         bpy.utils.unregister_class(cls)
     print("unregistered")
 
     #bpy.types.VIEW3D_MT_editor_menus.remove(draw_popover)
-
-if __name__ == "__main__":
-    register()
